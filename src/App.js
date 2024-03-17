69146d4f72a93e0c5d90087bd42b51049138f43b
JawadMM <almuttawa.j@hotmail.com>
2024-03-17 22:02:00 +0300

    created sigin in route

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router-dom";
 
 import Home from "./routes/home/Home";
 import Navigation from "./routes/navigation/Navigation";
+import SignIn from "./routes/sign-in/SignIn";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index={true} element={<Home />} />
+        <Route path="sign-in" element={<SignIn />} />
       </Route>
     </Routes>
   );
