926885deafada05ea35d6bd593cc0820b734fe5c
JawadMM <almuttawa.j@hotmail.com>
2024-03-17 21:04:37 +0300

    created navigation and home components with routing

--- src/App.js
+++ src/App.js
@@ -1,35 +1,16 @@
-import Categories from "./components/categories/categories.component";
+import { Route, Routes } from "react-router-dom";
+
+import Home from "./routes/home/home";
+import Navigation from "./routes/navigation/navigation";
 
 function App() {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    },
-  ];
-
-  return <Categories categories={categories} />;
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index={true} element={<Home />} />
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
---
+++ src/routes/navigation/navigation.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { Outlet, Link } from "react-router-dom";
+
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
+
+import "./navigation.styles.scss";
+
+const Navigation = () => {
+  return (
+    <>
+      <div className="navigation">
+        <Link className="logo-container" to={"/"}>
+          <CrwnLogo className="logo" />
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to={"/shop"}>
+            Shop
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </>
+  );
+};
+
+export default Navigation;
